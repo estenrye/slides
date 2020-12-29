5819f77a04f98843cc1668d9dbcf32185b53ac71
Esten Rye <esten.rye@ryezone.com>
2020-12-28 21:53:36 -0600

    add more etcd scripts

---
+++ 2020-12-11_Setting-Up-An-ETCD-Cluster/src/check-endpoint-status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Gets status of all cluster nodes in an etcd cluster.
+# RAFT TERM:  This is the current election term of the cluster.
+#             The Raft Term increments when a leader election occurs.
+
+# RAFT INDEX: This is the current committed raft entry index for the node.
+#             The raft index increments when a new entry is committed to the node.
+
+export REGISTRY="gcr.io/etcd-development/etcd"
+export TAG="latest"
+export CERT_DIR="/opt/etcd/certs"
+export SRV_RECORD="etcd.ryezone.com"
+
+docker run \
+  -e ETCDCTL_API=3 \
+  -v ${CERT_DIR}:/etcd-cert \
+  --rm \
+  ${REGISTRY}:${TAG} \
+  etcdctl \
+    --discovery-srv ${SRV_RECORD} \
+    --cert=/etcd-cert/etcd-client.crt \
+    --key=/etcd-cert/etcd-client.key \
+    --cacert=/etcd-cert/etcd-ca.crt \
+    endpoint status --cluster=true --write-out=table
---
+++ 2020-12-11_Setting-Up-An-ETCD-Cluster/src/check-etcd-endpoint-health.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Gets health of all cluster nodes in an etcd cluster.
+
+export REGISTRY="gcr.io/etcd-development/etcd"
+export TAG="latest"
+export CERT_DIR="/opt/etcd/certs"
+export SRV_RECORD="etcd.ryezone.com"
+
+docker run \
+  -e ETCDCTL_API=3 \
+  -v ${CERT_DIR}:/etcd-cert \
+  --rm \
+  ${REGISTRY}:${TAG} \
+  etcdctl \
+    --discovery-srv ${SRV_RECORD} \
+    --cert=/etcd-cert/etcd-client.crt \
+    --key=/etcd-cert/etcd-client.key \
+    --cacert=/etcd-cert/etcd-ca.crt \
+    --debug \
+    endpoint health --cluster=true
