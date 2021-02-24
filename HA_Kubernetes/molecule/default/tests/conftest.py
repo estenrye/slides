f679f1a3ce30dc9fd43a5b60c68ed04f769591ac
Esten Rye <esten.rye@ryezone.com>
2021-02-24 07:25:22 -0600

    clean up extra helper folder.

--- HA_Kubernetes/molecule/default/tests/conftest.py
+++ HA_Kubernetes/molecule/default/tests/conftest.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '../helpers'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../_test_helpers'))
 
 import pytest
 
