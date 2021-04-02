cd11031b6eadebb2fc6b47d7450d873f50e7ac61
Esten Rye <esten.rye@ryezone.com>
2021-04-01 23:00:52 -0500

    rotate certificates

--- HA_Kubernetes/_cluster/create-cluster.sh
+++ HA_Kubernetes/_cluster/create-cluster.sh
@@ -2,5 +2,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 kind create cluster --name local --config ${SCRIPT_DIR}/cluster.kind.yml
+kubectl apply -f ${SCRIPT_DIR}/manifests/allow-kublet-server-csrs.yml --context kind-local
 kubectl apply -f ${SCRIPT_DIR}/manifests/calico-cni.yml --context kind-local
 kubectl apply -f ${SCRIPT_DIR}/manifests/kube-bench.yml --context kind-local
+kubectl get job -w --context kind-local
