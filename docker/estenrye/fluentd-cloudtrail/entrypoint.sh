b1c374ff8c2426a0c8b28f604aed9ed9bef65191
Esten Rye <esten.rye@ryezone.com>
2022-09-25 17:18:02 -0500

    update image

--- docker/estenrye/fluentd-cloudtrail/entrypoint.sh
+++ docker/estenrye/fluentd-cloudtrail/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/dumb-init /bin/sh
 
+sniffer=$(fluent-gem contents fluent-plugin-elasticsearch|grep elasticsearch_simple_sniffer.rb)
+
 # If the user has supplied only arguments append them to `fluentd` command
 if [ "${1#-}" != "$1" ]; then
     set -- fluentd "$@"
@@ -14,6 +16,11 @@ if [ "$1" = "fluentd" ]; then
     if ! echo $@ | grep ' \-p' ; then
        set -- "$@" -p /fluentd/plugins
     fi
+
+    if ! echo $@ | grep '\-r'; then
+      set -- "$@" -r $sniffer
+    fi
 fi
 
+echo $@
 exec su-exec fluent "$@"
